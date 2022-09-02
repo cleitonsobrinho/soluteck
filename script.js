8912b5cf36b8f695472ab2431e5e2be154494ec9
Cleiton Sobrinho <cleiton1.life@gmail.com>
2022-09-02 00:09:42 -0300

    projeto energiar

---
+++ script.js
@@ -0,0 +1,40 @@
+
+
+
+//Menu mobile
+
+const btnMobile = document.getElementById('btn-mobile')
+
+function toggleMenu() {
+  const nav = document.getElementById('nav')
+  nav.classList.toggle('active')
+}
+
+btnMobile.addEventListener('click', toggleMenu)
+
+
+
+// automatic slide effect
+
+let dots = document.getElementsByClassName("dot");
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1 }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 8000); // Change image every 8 seconds
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+
+}
